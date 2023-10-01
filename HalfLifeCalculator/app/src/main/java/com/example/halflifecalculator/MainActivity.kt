5584eac74f13063e7643265f95fcd185b5f430b1
Mechanical Max <93453184+MechanicalMax@users.noreply.github.com>
2023-10-01 16:39:53 -0400

    Insert Project Folder

---
+++ HalfLifeCalculator/app/src/main/java/com/example/halflifecalculator/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.halflifecalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+
+    }
+}
